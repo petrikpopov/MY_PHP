250cf36fe65c0697cd9b5dfbbf6f90c0f53574b3
Petrik <112963523+petrikpopov@users.noreply.github.com>
2024-12-02 13:49:41 +0200

    Add files via upload

---
+++ PHP/index.php
@@ -0,0 +1,14 @@
+<?php
+// echo 'Hello World'.PHP_EOL;
+// var_dump(20).PHP_EOL;
+// print_r(2).PHP_EOL;
+// print_r([1,2,3,4,5]);
+// echo gettype("Hello");
+// echo PHP_EOL;
+
+$count = 15;
+$price = 25;
+$name = "Сухарики";
+$isSuccess = true;
+echo "Title:$name, Price:$price, Count:$count"."\n";
+
